b834d8cd031de01e80810cfc7e46e3334c8b5341
parvez <parvezmusharraf61@gmail.com>
2023-07-22 14:12:58 +0530

    Added footer section social media link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,19 @@ import  '../Footer/footer.scss'
 export default function Footer() {
   return (
     <div className='footer'>
+      <div className="heading">
+        <h5>Connect Me with</h5>
+        <i class="fa-solid fa-down-long"></i>
+      </div>
+    <div class="icons" >
+    <a href="https://github.com/"><i class="fa-brands fa-github"></i></a>
+    <a href="https://twitter.com/"><i class="fa-brands fa-twitter "></i></a>
+    <a href="https://www.linkedin.com/"><i class="fa-brands fa-linkedin"></i></a>
+    <a href="https://www.linkedin.com/"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+    <div className="footer">
           &copy; 2023 My Portfolio. All rights reserved.
+    </div>
     </div>
   )
 }
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,14 +7,14 @@ export default function Services() {
     <section class="pro_services" id="pro_services">
       <div class="service">
         <p>
-          <i class="fa-brands fa-html5 fa-3x "></i>
+        <i class="fa-brands fa-html5 fa-3x "></i>
           <i class="fa-brands fa-css3-alt fa-3x"></i>
         </p>
         <h3>Html+Css development</h3>
         <p>Design Using Html Css </p>
       </div>
       <div class="service">
-        <i class="fa-brands fa-square-js fa-3x"></i>
+      <i class="fa-brands fa-square-js fa-3x"></i>
         <h3>JavaScript Development</h3>
         <p>Experties in javascript </p>
       </div>
