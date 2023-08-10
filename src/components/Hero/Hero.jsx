9958450c275b5c02ae2107cce36aa34cec580aaa
parvez <parvezmusharraf61@gmail.com>
2023-08-11 00:04:00 +0530

    Added animation on hero page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './hero.scss'
 export default function Hero(props) {
   return (
     <div className='hero' id='hero'>
-      <div className="text" id="text" >
+      <div className="text focus-in-expand-fwd" id="text" >
         <h2>Hello, My name is</h2>
         <h1>{props.name}</h1>
         <p>and I am {props.role} with passion for building beautiful and user-friendly website   . </p>
