4c38d9257dbc242bd18f13bfae8a79c55af71424
parvez <parvezmusharraf61@gmail.com>
2023-08-23 11:50:26 +0530

    Added animation to whole  Site

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
 import '../Footer/footer.scss'
+import { useEffect,useRef } from 'react';
+import {motion,useInView,useAnimation } from "framer-motion";
 
 export default function Footer() {
+  const ref = useRef(null);
+  const isInView = useInView(ref,{once:true});
+  
+  const mainControls = useAnimation();
+  const slidesControls = useAnimation();
+  
+    useEffect(()=>{
+      if(isInView){
+        mainControls.start("visible");
+        slidesControls.start("visible");
+      }
+    },)
+
   return (
-    <div className='footer'>
+    <motion.div ref={ref} className='footer'  variants={{
+      hidden:{opacity:0,y:75},
+      visible:{opacity:1, y:0}
+    }}
+    initial="hidden"
+            animate={mainControls}
+            transition={{duration:0.5,delay:0.34}}>
       <div className="heading">
         <h4>Follow Me</h4>
         <i title="Connect" className="fa-solid fa-down-long"></i>
@@ -21,6 +42,6 @@ export default function Footer() {
       <div className="CopyRight">
         &copy; 2023 My Portfolio. All rights reserved.
       </div>
-    </div>
+    </motion.div>
   )
 }
