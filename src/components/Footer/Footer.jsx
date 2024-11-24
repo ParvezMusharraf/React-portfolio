134d53b434b5b2c9293bd90d4e6b32b3a2ca0eef
Parvez Musharraf <parvez.musharraf@pixonix.tech>
2024-11-24 23:56:08 +0530

    Added resume

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import '../Footer/footer.scss'
 import { useEffect,useRef } from 'react';
 import {motion,useInView,useAnimation } from "framer-motion";
+import helper from '../../resume/demopdt.pdf'
 
 export default function Footer() {
   const ref = useRef(null);
@@ -16,6 +17,19 @@ export default function Footer() {
       }
     },)
 
+    // const fileUrl = '/assets/helppdf.pdf'; // Make sure the path matches where you placed the file
+
+
+    const handleDownload = () => {
+      // Create a temporary link element
+      const link = document.createElement('a');
+      link.href = helper;
+      link.download = 'helppdf.pdf'; // Specify the name of the downloaded file
+      document.body.appendChild(link); // Append the link to the body
+      link.click(); // Trigger the click event to start the download
+      document.body.removeChild(link); // Remove the link from the body
+    };
+
   return (
     <motion.div ref={ref} className='footer'  variants={{
       hidden:{opacity:0,y:75},
@@ -26,6 +40,16 @@ export default function Footer() {
             transition={{duration:0.5,delay:0.34}}>
       <div className="heading">
         <h4>Follow Me</h4>
+        <iframe
+        src={helper}
+        width="100%"
+        height="600px"
+        title="Help Document"
+        style={{ border: 'none' }}
+      />
+      <a href={helper} download="helppdf.pdf"/>
+        <button>Download PDF</button>
+        <button onClick={handleDownload} >Download Resume</button>
         <i title="Connect" className="fa-solid fa-down-long"></i>
       </div>
       <div className="icons" >
