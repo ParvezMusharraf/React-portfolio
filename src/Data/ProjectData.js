18da8c826335f0db42b7d7195814761e92cfc038
Parvez Musharraf <parvez.musharraf@pixonix.tech>
2025-06-02 16:33:19 +0530

    Project Updated

--- src/Data/ProjectData.js
+++ src/Data/ProjectData.js
@@ -1,133 +1,235 @@
-import Portfolio from '../Images/PortFolio.png'
-import flyFinder from '../Images/Untitled.png'
-import TrueSqft from '../Images/true sqft.jpg'
-
+import Portfolio from "../Images/PortFolio.png";
+import flyFinder from "../Images/Untitled.png";
+import TrueSqft from "../Images/true sqft.jpg";
+import Shippyfi from "../Images/Shippyfi.png";
+import MDM from "../Images/mdm.webp";
 
 const ProjectData = [
-    {
+  {
+    id: 1,
+    Live: "https://parvezmusharraf.github.io/React-portfolio/",
+    name: "Personal Portfolio",
+    repoLink: "https://github.com/ParvezMusharraf/React-portfolio",
+    TechnologyUsed: [
+      {
         id: 1,
-        Live: 'https://parvezmusharraf.github.io/React-portfolio/',
-        name: "Personal Portfolio",
-        repoLink: 'https://github.com/ParvezMusharraf/React-portfolio',
-        TechnologyUsed: [
-            {
-                id: 1,
-                name: "Html",
-                icon:"fa-brands fa-html5",
-                color:"rgb(255 133 59)"
-            },
-            {
-                id: 2,
-                name: "css",
-                icon:"fa-brands fa-css3",
-                color:"#2b7cf2"
-            },
-            {
-                id: 3,
-                name: "javascript",
-                icon:"fa-brands fa-js",
-                color:"#FFD43B"
-            },
-            {
-                id: 5,
-                name: "Bootstrap",
-                icon:"fa-brands fa-bootstrap",
-                color:"rgb(59 95 255)"
-            },
-            {
-                id: 6,
-                name: "React",
-                icon:"fa-brands fa-react",
-                color:"rgb(48 224 220)"
-            },
-        ],
-        // TechnologyUsed: "Html , Scss , Javascript , Bootstrap , React.",
-        img: Portfolio,
-    },
-    {
+        name: "Html",
+        icon: "fa-brands fa-html5",
+        color: "rgb(255 133 59)",
+      },
+      {
         id: 2,
-        Live: 'https://parvezmusharraf.github.io/Fly_Finders-react/',
-        name: 'Flight Booking System ',
-        repoLink: 'https://github.com/ParvezMusharraf/Fly_Finders-react/',
-        TechnologyUsed:[
-            {
-                id: 1,
-                name: "Html",
-                icon:"fa-brands fa-html5",
-                color:"rgb(255 133 59)"
-            },
-            {
-                id: 2,
-                name: "css",
-                icon:"fa-brands fa-css3",
-                color:"#2b7cf2"
-            },
-            {
-                id: 3,
-                name: "javascript",
-                icon:"fa-brands fa-js",
-                color:"#FFD43B"
-            },
-            {
-                id: 5,
-                name: "Bootstrap",
-                icon:"fa-brands fa-bootstrap",
-                color:"rgb(59 95 255)"
-            },
-            {
-                id: 6,
-                name: "React",
-                icon:"fa-brands fa-react",
-                color:"rgb(48 224 220)"
-            },
-        ],
-        img: flyFinder,
-    },
-    {
+        name: "css",
+        icon: "fa-brands fa-css3",
+        color: "#2b7cf2",
+      },
+      {
         id: 3,
-        Live: 'https://parvezmusharraf.github.io/Fly_Finders-react/',
-        name: 'True Square Feet',
-        repoLink: 'https://github.com/ParvezMusharraf/Fly_Finders-react/',
-        TechnologyUsed:[
-            {
-                id: 1,
-                name: "Html",
-                icon:"fa-brands fa-html5",
-                color:"rgb(255 133 59)"
-            },
-            {
-                id: 2,
-                name: "css",
-                icon:"fa-brands fa-css3",
-                color:"#2b7cf2"
-            },
-            {
-                id: 3,
-                name: "javascript",
-                icon:"fa-brands fa-js",
-                color:"#FFD43B"
-            },
-            {
-                id: 5,
-                name: "Bootstrap",
-                icon:"fa-brands fa-bootstrap",
-                color:"rgb(59 95 255)"
-            },
-            {
-                id: 6,
-                name: "React",
-                icon:"fa-brands fa-react",
-                color:"rgb(48 224 220)"
-            },
-            {
-                id: 7,
-                name: "Reatful Apis",
-                icon:"fa-solid fa-code-pull-request",
-                color:"#86b7fe"
-            },
-        ],      
-        img: TrueSqft,
-    },
-]
+        name: "javascript",
+        icon: "fa-brands fa-js",
+        color: "#FFD43B",
+      },
+      {
+        id: 5,
+        name: "Bootstrap",
+        icon: "fa-brands fa-bootstrap",
+        color: "rgb(59 95 255)",
+      },
+      {
+        id: 6,
+        name: "React",
+        icon: "fa-brands fa-react",
+        color: "rgb(48 224 220)",
+      },
+    ],
+    img: Portfolio,
+  },
+  {
+    id: 2,
+    Live: "https://parvezmusharraf.github.io/Fly_Finders-react/",
+    name: "Flight Booking System ",
+    repoLink: "https://github.com/ParvezMusharraf/Fly_Finders-react/",
+    TechnologyUsed: [
+      {
+        id: 1,
+        name: "Html",
+        icon: "fa-brands fa-html5",
+        color: "rgb(255 133 59)",
+      },
+      {
+        id: 2,
+        name: "css",
+        icon: "fa-brands fa-css3",
+        color: "#2b7cf2",
+      },
+      {
+        id: 3,
+        name: "javascript",
+        icon: "fa-brands fa-js",
+        color: "#FFD43B",
+      },
+      {
+        id: 5,
+        name: "Bootstrap",
+        icon: "fa-brands fa-bootstrap",
+        color: "rgb(59 95 255)",
+      },
+      {
+        id: 6,
+        name: "React",
+        icon: "fa-brands fa-react",
+        color: "rgb(48 224 220)",
+      },
+    ],
+    img: flyFinder,
+  },
+  {
+    id: 3,
+    Live: "https://parvezmusharraf.github.io/Fly_Finders-react/",
+    name: "True Square Feet",
+    repoLink: "https://github.com/ParvezMusharraf/Fly_Finders-react/",
+    TechnologyUsed: [
+      {
+        id: 1,
+        name: "Html",
+        icon: "fa-brands fa-html5",
+        color: "rgb(255 133 59)",
+      },
+      {
+        id: 2,
+        name: "css",
+        icon: "fa-brands fa-css3",
+        color: "#2b7cf2",
+      },
+      {
+        id: 3,
+        name: "javascript",
+        icon: "fa-brands fa-js",
+        color: "#FFD43B",
+      },
+      {
+        id: 5,
+        name: "Bootstrap",
+        icon: "fa-brands fa-bootstrap",
+        color: "rgb(59 95 255)",
+      },
+      {
+        id: 6,
+        name: "React",
+        icon: "fa-brands fa-react",
+        color: "rgb(48 224 220)",
+      },
+      {
+        id: 7,
+        name: "Reatful Apis",
+        icon: "fa-solid fa-code-pull-request",
+        color: "#86b7fe",
+      },
+    ],
+    img: TrueSqft,
+  },
+  {
+    id: 3,
+    Live: "https://shippyfi.com/",
+    name: "Shippyfi",
+    // repoLink: "https://github.com/ParvezMusharraf/Fly_Finders-react/",
+    TechnologyUsed: [
+      {
+        id: 1,
+        name: "Html",
+        icon: "fa-brands fa-html5",
+        color: "rgb(255 133 59)",
+      },
+      {
+        id: 2,
+        name: "css",
+        icon: "fa-brands fa-css3",
+        color: "#2b7cf2",
+      },
+      {
+        id: 3,
+        name: "javascript",
+        icon: "fa-brands fa-js",
+        color: "#FFD43B",
+      },
+      {
+        id: 5,
+        name: "Bootstrap",
+        icon: "fa-brands fa-bootstrap",
+        color: "rgb(59 95 255)",
+      },
+      {
+        id: 6,
+        name: "React",
+        icon: "fa-brands fa-react",
+        color: "rgb(48 224 220)",
+      },
+      {
+        id: 7,
+        name: "Reatful Apis",
+        icon: "fa-solid fa-code-pull-request",
+        color: "#86b7fe",
+      },
+      {
+        id: 8,
+        name: "JWT",
+        icon: "fa-solid fa-key",
+        color: "rgb(48 224 220)",
+      },
+    ],
+    img: Shippyfi,
+  },
+  {
+    id: 4,
+    Live: "https://mdmadminpanel.safe4sure.ai/",
+    name: "MDM(Mobile Device Management)",
+    // repoLink: "https://github.com/ParvezMusharraf/Fly_Finders-react/",
+    TechnologyUsed: [
+      {
+        id: 1,
+        name: "Html",
+        icon: "fa-brands fa-html5",
+        color: "rgb(255 133 59)",
+      },
+      {
+        id: 2,
+        name: "css",
+        icon: "fa-brands fa-css3",
+        color: "#2b7cf2",
+      },
+      {
+        id: 3,
+        name: "Typescript",
+        icon: "fa-brands fa-js",
+        color: "#FFD43B",
+      },
+      {
+        id: 5,
+        name: "Bootstrap",
+        icon: "fa-brands fa-bootstrap",
+        color: "rgb(59 95 255)",
+      },
+      {
+        id: 6,
+        name: "React",
+        icon: "fa-brands fa-react",
+        color: "rgb(48 224 220)",
+      },
+      {
+        id: 7,
+        name: "Reatful Apis",
+        icon: "fa-solid fa-code-pull-request",
+        color: "#86b7fe",
+      },
+      {
+        id: 8,
+        name: "JWT",
+        icon: "fa-solid fa-key",
+        color: "rgb(48 224 220)",
+      },
+    ],
+    img: MDM,
+  },
+];
 
-export default ProjectData;
+export default ProjectData.reverse();
